3910b8ca1dedc7daad8cd2f112baedd1341c4347
ahmad <ahmad_mahmoud@gmail.com>
2022-12-26 20:42:22 +0200

    Routes Tasks

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $pageTitle = 'Homepage';
+
+        return view('welcome', compact('pageTitle'));
+    }
+}
